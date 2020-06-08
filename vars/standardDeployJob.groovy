d0ed4e7fe1b1b11414c6bb61fcafc75e2f858e53
TomJKing <tom.king@nationalarchives.gov.uk>
2020-06-08 08:07:10 +0100

    Add params

--- vars/standardDeployJob.groovy
+++ vars/standardDeployJob.groovy
@@ -3,6 +3,9 @@ def call(Map params) {
         agent {
             label "master"
         }
+        parameters {
+            choice(name: "STAGE", choices: ["intg", "staging", "prod"], description: "The stage you are building the auth server for")
+        }
         stages {
             stage("Docker") {
                 agent {
@@ -10,49 +13,8 @@ def call(Map params) {
                 }
                 steps {
                     script {
-                        docker.withRegistry('', 'docker') {
-                            sh "docker pull nationalarchives/${params.imageName}:${params.toDeploy}"
-                            sh "docker tag nationalarchives/${params.imageName}:${params.toDeploy} nationalarchives/${params.imageName}:${params.stage}"
-                            sh "docker push nationalarchives/${params.imageName}:${params.stage}"
-
-                            slackSend color: "good", message: "*${params.imageName}* :whale: The '${params.toDeploy}' image has been tagged with '${params.stage}' in Docker Hub", channel: "#tdr-releases"
-                        }
-                    }
-                }
-            }
-            stage("Update ECS container") {
-                agent {
-                    ecs {
-                        inheritFrom "aws"
-                        taskrole "arn:aws:iam::${env.MANAGEMENT_ACCOUNT}:role/TDRJenkinsNodeRole${params.stage.capitalize()}"
-                    }
-                }
-                steps {
-                    script {
-                        def accountNumber = tdr.getAccountNumberFromStage("${params.stage}")
-
-                        sh "python3 /update_service.py ${accountNumber} ${params.stage} ${params.eCSService}"
-                        slackSend color: "good", message: "*${params.eCSService}* :arrow_up: The app has been updated in ECS in the *${params.stage}* environment", channel: "#tdr-releases"
-                    }
-                }
-            }
-            stage("Update release branch") {
-                agent {
-                    label "master"
-                }
-                steps {
-                    sh "git branch -f ${params.releaseBranch} HEAD"
-                    sshagent(['github-jenkins']) {
-                        sh("git push -f origin ${params.releaseBranch}")
-                    }
-                }
-            }
-        }
-        post {
-            success {
-                script {
-                    if (params.stage == "intg") {
-                        tdr.runEndToEndTests(300, "${params.stage}", BUILD_URL)
+                        echo "STAGE: ${params.STAGE}"
+                        slackSend color: "good", message: "*${params.imageName}* :whale: The '${params.toDeploy}' image has been tagged with '${params.stage}' in Docker Hub", channel: "#bot-testing"
                     }
                 }
             }
